b6cdea5fb1cef8af914d7642aa2f9317d1705c34
greenTransistor <artur.viligurskyi@gmail.com>
2019-02-27 14:36:25 +0200

    changed main to work with all input files

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 using namespace std;
 
-const int MAX_SIZE = 1001;
+//Configurable, more = better result but worse performance (1000 for big is 65 seconds on Intel Core i5-7200U)
 const int ATTEMPTS_COUNT = 10;
+
+const int FILES_COUNT = 4;
+const string FILE_NAMES[FILES_COUNT] = {"a_example", "b_small", "c_medium", "d_big"};
+const int MAX_SIZE = 1001;
 
 struct Rect {
 	int x0, y0, x1, y1;
@@ -28,26 +32,30 @@ vector<Rect> answer;
 #include "find_optimal_slices.cpp"
 
 int main() {
-	ifstream inf("a_example.in");
-	//ifstream inf("b_small.in");
-	//ifstream inf("c_medium.in");
-	//ifstream inf("d_big.in");
-	inf >> r >> c >> l >> h;
-	for (int i = 0; i < r; i++) {
-		inf >> a[i];
-	}
-	inf.close();
+	string fileName, inputFileName, outputFileName;
+	for (int fileIndex = 0; fileIndex < FILES_COUNT; fileIndex++) {
+		fileName = FILE_NAMES[fileIndex];
+		inputFileName = fileName + ".in";
+		outputFileName = fileName + ".out";
 
-	calcSum();
-	findBestRects();
-	findOptimalSlices();
+		ifstream inf(inputFileName);
+		inf >> r >> c >> l >> h;
+		for (int i = 0; i < r; i++) {
+			inf >> a[i];
+		}
+		inf.close();
 
-	ofstream outf("output.txt");
-	outf << answer.size() << "\n";
-	for (int i = 0; i < answer.size(); i++) {
-		outf << answer[i].y0 << " " << answer[i].x0 << " " << answer[i].y1 << " " << answer[i].x1 << "\n";
+		calcSum();
+		findBestRects();
+		findOptimalSlices();
+
+		ofstream outf(outputFileName);
+		outf << answer.size() << "\n";
+		for (int i = 0; i < answer.size(); i++) {
+			outf << answer[i].y0 << " " << answer[i].x0 << " " << answer[i].y1 << " " << answer[i].x1 << "\n";
+		}
+		outf.close();
 	}
-	outf.close();
 
 	return 0;
 }
