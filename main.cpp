4d35f2053f925d6a15e569196504e602f4008dd5
greenTransistor <artur.viligurskyi@gmail.com>
2019-02-27 12:23:05 +0200

    added Rect structure and more variables

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 const int MAX_SIZE = 1001;
 
+struct Rect {
+	int x0, x1, y0, y1;
+};
+
 int r, c, l, h;
 char a[MAX_SIZE][MAX_SIZE];
+vector<Rect> rects[MAX_SIZE][MAX_SIZE];
+vector<Rect> answer;
+
+#include "partialSums.cpp"
 
 int main() {
 	ifstream inf("a_example.in");
@@ -14,6 +23,9 @@ int main() {
 	//ifstream inf("c_medium.in");
 	//ifstream inf("d_big.in");
 	inf >> r >> c >> l >> h;
+	for (int i = 0; i < r; i++) {
+		inf >> a[i];
+	}
 	inf.close();
 
 	return 0;
